2a660a0fac1d003da65e760413b455000dfa2942
Ching-Hsin Lee <chinglee@amazon.com>
2021-02-05 10:36:08 +0800

    Remove unused esp32 arduino modules
    Remove unused esp32 arduino modules

--- cores/esp32/wiring_pulse.c
+++ cores/esp32/wiring_pulse.c
@@ -14,10 +14,12 @@
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-//#include <limits.h>
 #include "wiring_private.h"
 #include "pins_arduino.h"
 
+#ifndef UINT_MAX
+    #include <limits.h>
+#endif
 
 extern uint32_t xthal_get_ccount();
 
